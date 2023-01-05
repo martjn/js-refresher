7693b22d8842ed649898fdee295838d9c5e9d49e
martjn <mingdev77@gmail.com>
2023-01-05 17:17:54 +0200

    import & export

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("yo");
+import person from "./person.js";
+import prsn from "./person.js";
+import { cleanOutput } from './utils.js';
+import { data } from './utils.js';
+
+import { cleanOutput as clean } from './utils.js'
+
+console.log(person)
+console.log(prsn)
+console.log(data)
+
+cleanOutput()
+console.log(data)
+clean()
+
+cleanOutput()
+
+console.log(utils)
+console.log(utils.data)
